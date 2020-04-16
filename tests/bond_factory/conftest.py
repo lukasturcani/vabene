30c17c6bd8a9e47aba44422b12708a6dde9935fc
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-16 09:53:59 +0200

    Added test case.

--- tests/bond_factory/conftest.py
+++ tests/bond_factory/conftest.py
@@ -1,8 +1,30 @@
 import pytest
+import vabene as vb
+
+from .case_data import CaseData
 
 
 @pytest.fixture(
     params=(
+        CaseData(
+            bond_factory=vb.RandomBondFactory(
+                max_internal_bonds=0,
+                required_bonds=(
+                    vb.Bond(0, 1, 2),
+                    vb.Bond(0, 2, 1),
+                ),
+                random_seed=None,
+            ),
+            atoms=(
+                vb.Atom(6, 0, 4),
+                vb.Atom(6, 0, 4),
+                vb.Atom(6, 0, 4),
+            ),
+            bonds=(
+                vb.Bond(0, 1, 2),
+                vb.Bond(0, 2, 1),
+            ),
+        ),
     ),
 )
 def case_data(request):
