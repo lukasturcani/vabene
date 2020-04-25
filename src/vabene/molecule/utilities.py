4e79a34ec710125b226baeedbe42bcf54aa54dd6
Lukas Turcani <lukasturcani93@gmail.com>
2020-04-25 16:31:43 +0200

    Added docstring. (#2)

--- src/vabene/molecule/utilities.py
+++ src/vabene/molecule/utilities.py
@@ -1,4 +1,23 @@
 def dedupe(iterable, get_key):
+    """
+    Yield from `iterable` without duplicates.
+
+    Parameters
+    ----------
+    iterable : :class:`iterable`
+        The iterable whose items should be deduped.
+
+    get_key : :class:`callable`
+        Takes an item from `iterable` and a key. If two items return
+        the same key, they are considered duplicates.
+
+    Yields
+    ------
+    :class:`object`
+        An item in `iterable`.
+
+    """
+
     seen = set()
     for item in iterable:
         key = get_key(item)
